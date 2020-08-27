8de00bc23d271813c0963fab112a12c6dc3a8e19
James Robertson <jamesurobertson@gmail.com>
2020-08-27 10:12:56 -0500

    added comments. reorganized

--- public/main.js
+++ public/main.js
@@ -2,11 +2,24 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 const socket = io();
 
+
 const playerRadius = 10;
+
+//initial Target details
 let targetX = canvas.width / 2;
 let targetY = canvas.height / 2;
-const target = 10;
+const targetRadius = 10;
 
+// when there is a new connection emit new player
+socket.emit("new player", {
+  x: Math.random() * canvas.width,
+  y: Math.random() * canvas.height,
+  targetX,
+  targetY,
+});
+
+
+// player movement mapping
 const keys = {
   left: false,
   up: false,
@@ -17,76 +30,94 @@ const keys = {
 document.addEventListener("keyup", (event) => keyHandler(event, false));
 document.addEventListener("keydown", (event) => keyHandler(event, true));
 
-function keyHandler(event, bool) {
-  const kc = event.keyCode;
 
-  if (kc === 37) keys.left = bool;
-  if (kc === 38) keys.up = bool;
+// key presses update key movement
+function keyHandler(event, bool) {
+    const kc = event.keyCode;
+
+    if (kc === 37) keys.left = bool;
+    if (kc === 38) keys.up = bool;
   if (kc === 39) keys.right = bool;
   if (kc === 40) keys.down = bool;
 }
 
-socket.emit("new player", { x: Math.random() * canvas.width, y: Math.random() *canvas.height, targetX, targetY});
+// emit player movement at 60 fps
+// TODO: Fix Lag. Does it have to do with this?
 setInterval(() => socket.emit("movement", keys), 1000 / 60);
 
 function drawPlayer(player) {
-  ctx.beginPath();
-  ctx.arc(player.x, player.y, playerRadius, 0, Math.PI * 2);
-  ctx.fillStyle = player.color
+    ctx.beginPath();
+    ctx.arc(player.x, player.y, playerRadius, 0, Math.PI * 2);
+  ctx.fillStyle = player.color;
   ctx.fill();
   ctx.closePath();
 }
 
-
-function drawtarget() {
+function drawtarget(target) {
+  if (!target) return;
   ctx.beginPath();
-  ctx.arc(targetX, targetY, target, 0, Math.PI * 2);
+  ctx.arc(target.x, target.y, targetRadius, 0, Math.PI * 2);
   ctx.fillStyle = "black";
   ctx.fill();
   ctx.closePath();
 }
 
 function drawScores(players) {
-    let start = 20
-    ctx.font = "17px Helvetica";
-    ctx.fillStyle = "black";
-    ctx.fillText("Leaderboard", 490, start);
-    if (!players) return
-    start += 25
-    let playersArr = []
-    for (let id in players) {
-        playersArr.push(players[id])
-    }
+  if (!players) return;
 
-    playersArr.sort((a, b) => b.score - a.score)
-    const len = playersArr.length > 5 ? 5 : playersArr.length
-    playersArr.slice(0, len).forEach((player, i) => {
-        ctx.font = "15px Helvetica";
-        ctx.fillStyle = player.color
-        ctx.fillText(`${i + 1}. ` + player.score, 480, start);
-        start += 15
-    })
+  // x index of where the leaderboard starts drawing
+  let start = 20;
 
-    const activePlayer = players[socket.id]
-    if (!playersArr.slice(0, 5).includes(activePlayer)) {
-        ctx.font = "15px Helvetica";
-        ctx.fillStyle = activePlayer.color
-        ctx.fillText(`${playersArr.indexOf(activePlayer) + 1}. ` + activePlayer.score, 480, start);
-    }
+  // drawing 'Leaderboard'
+  ctx.font = "17px Helvetica";
+  ctx.fillStyle = "black";
+  ctx.fillText("Leaderboard", 490, start);
+  start += 25;
+
+  // push player objects into an array
+  let playersArr = [];
+  for (let id in players) {
+    playersArr.push(players[id]);
+  }
+
+  // sort by score
+  playersArr.sort((a, b) => b.score - a.score);
+
+  // we will only display top 5 scores on the leaderboard
+  const len = playersArr.length > 5 ? 5 : playersArr.length;
+  playersArr.slice(0, len).forEach((player, i) => {
+    ctx.font = "15px Helvetica";
+    ctx.fillStyle = player.color;
+    ctx.fillText(`${i + 1}. ` + player.score, 480, start);
+    start += 15;
+  });
+
+  // if activePlayer isn't on leaderboard, show their score
+  const activePlayer = players[socket.id];
+  if (!playersArr.slice(0, 5).includes(activePlayer)) {
+    ctx.font = "15px Helvetica";
+    ctx.fillStyle = activePlayer.color;
+    ctx.fillText(
+      `${playersArr.indexOf(activePlayer) + 1}. ` + activePlayer.score,
+      480,
+      start
+    );
+  }
 }
 
 function clearCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
+
+
+// Canvas Refreshing when receiving updated state'
 socket.on("state", (players, target) => {
   clearCanvas();
   for (let id in players) {
     const player = players[id];
     drawPlayer(player);
   }
-  if (target) targetX = target.x
-  if (target) targetY = target.y
-  drawScores(players)
-  drawtarget();
+  drawScores(players);
+  drawtarget(target);
 });
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+const colors = require('./colors')
 
 app.use(express.static(__dirname + "/public"));
 
@@ -9,109 +10,13 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-const colors = [
-  "Navy",
-  "MediumBlue",
-  "Blue",
-  "DarkGreen",
-  "Green",
-  "Teal",
-  "DarkCyan",
-  "DeepSkyBlue",
-  "DarkTurquoise",
-  "MediumSpringGreen",
-  "Lime",
-  "SpringGreen",
-  "Aqua",
-  "Cyan",
-  "DodgerBlue",
-  "LightSeaGreen",
-  "ForestGreen",
-  "SeaGreen",
-  "DarkSlateGray",
-  "DarkSlateGrey",
-  "LimeGreen",
-  "MediumSeaGreen",
-  "Turquoise",
-  "RoyalBlue",
-  "SteelBlue",
-  "DarkSlateBlue",
-  "MediumTurquoise",
-  "Indigo  ",
-  "DarkOliveGreen",
-  "CadetBlue",
-  "CornflowerBlue",
-  "RebeccaPurple",
-  "MediumAquaMarine",
-  "DimGray",
-  "DimGrey",
-  "SlateBlue",
-  "OliveDrab",
-  "SlateGray",
-  "SlateGrey",
-  "LightSlateGray",
-  "LightSlateGrey",
-  "MediumSlateBlue",
-  "LawnGreen",
-  "Chartreuse",
-  "Aquamarine",
-  "Maroon",
-  "Purple",
-  "Olive",
-  "Gray",
-  "Grey",
-  "SkyBlue",
-  "LightSkyBlue",
-  "BlueViolet",
-  "DarkRed",
-  "DarkMagenta",
-  "SaddleBrown",
-  "DarkSeaGreen",
-  "LightGreen",
-  "MediumPurple",
-  "DarkViolet",
-  "PaleGreen",
-  "DarkOrchid",
-  "YellowGreen",
-  "Sienna",
-  "Brown",
-  "DarkGray",
-  "DarkGrey",
-  "LightBlue",
-  "GreenYellow",
-  "PaleTurquoise",
-  "LightSteelBlue",
-  "PowderBlue",
-  "FireBrick",
-  "DarkGoldenRod",
-  "MediumOrchid",
-  "RosyBrown",
-  "DarkKhaki",
-  "Silver",
-  "MediumVioletRed",
-  "IndianRed ",
-  "Peru",
-  "Chocolate",
-  "Tan",
-  "LightGray",
-  "LightGrey",
-  "Thistle",
-  "Orchid",
-  "GoldenRod",
-  "PaleVioletRed",
-  "Crimson",
-  "Gainsboro",
-  "Plum",
-  "BurlyWood",
-  "LightCyan",
-  "Lavender",
-  "DarkSalmon",
-  "Violet",
-];
+// players and target objects
 const players = {};
 const target = { x: 0, y: 0 };
+
 io.on("connection", (socket) => {
-  let [canvasWidth, canvasHeight] = [600, 400];
+  let canvasWidth = 600
+  let canvasHeight = 400
 
   socket.on("disconnect", () => {
     delete players[socket.id];
@@ -130,11 +35,9 @@ io.on("connection", (socket) => {
     io.emit("state", players);
   });
 
-  const lastUpdatedTime = new Date().getTime();
+  //TODO: optimize this. Cause for lag?
   socket.on("movement", (data) => {
     const player = players[socket.id] || {};
-    const currentTime = new Date().getTime();
-    const timeDiff = currentTime - lastUpdatedTime;
     if (data.left) player.x -= 4;
     if (data.right) player.x += 4;
     if (data.up) player.y -= 4;
